d7a1637d9ea84053f09a49c743be397bc9cb10e3
Jansen-Alves <75702771+Jansen-Alves@users.noreply.github.com>
2023-10-12 11:26:36 -0300

    Update clientes.h

--- clientes.h
+++ clientes.h
@@ -1,9 +1,11 @@
-typedef struct Cliente
-{
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+typedef struct Cliente{
     int chave;
     char nome[100];
     int estado;
     int prox;
 }Cliente;
 
-
+#endif
